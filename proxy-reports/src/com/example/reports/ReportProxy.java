74f44e2e6d9751dc7d055afb08e1e2c3afef21b5
Rabari9999 <rabarikrishramjibhai2905@gmail.com>
2026-03-25 12:05:42 +0530

    Done with Proxy-report assignment

--- proxy-reports/src/com/example/reports/ReportProxy.java
+++ proxy-reports/src/com/example/reports/ReportProxy.java
@@ -13,6 +13,7 @@ public class ReportProxy implements Report {
     private final String title;
     private final String classification;
     private final AccessControl accessControl = new AccessControl();
+    private RealReport report;
 
     public ReportProxy(String reportId, String title, String classification) {
         this.reportId = reportId;
@@ -24,7 +25,18 @@ public class ReportProxy implements Report {
     public void display(User user) {
         // Starter placeholder: intentionally incorrect.
         // Students should remove direct real loading on every call.
-        RealReport report = new RealReport(reportId, title, classification);
+
+                    // authentication
+        if(!accessControl.canAccess(user, classification)) 
+        {
+            System.out.println("Access denied for user : " + user.getName());
+        }
+        
+                    // lazy loading 
+        if(report == null)
+        {
+            report = new RealReport(reportId, title, classification);
+        }
         report.display(user);
     }
 }
